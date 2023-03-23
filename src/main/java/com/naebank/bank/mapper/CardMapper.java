ed1453d73402f73074dad3ab3643f600f843254f
DenysLobur <denismachtalles@gmail.com>
2023-03-24 01:18:37 +0200

    Fix missing amount in Card MapStruct

--- src/main/java/com/naebank/bank/mapper/CardMapper.java
+++ src/main/java/com/naebank/bank/mapper/CardMapper.java
@@ -3,14 +3,16 @@ package com.naebank.bank.mapper;
 import com.naebank.bank.controller.dto.CardDto;
 import com.naebank.bank.repository.entity.CardEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface CardMapper {
     CardMapper INSTANCE = Mappers.getMapper(CardMapper.class);
 
+    @Mapping(target = "amount", source = "amount")
     CardDto toDto(CardEntity cardEntity);
 
     CardEntity toEntity(CardDto cardDto);
